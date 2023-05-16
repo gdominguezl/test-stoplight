624021a07e4b2b1ea30106fdc554b49f9162e62e
gustavo.dominguez@leniolabs.com <gustavo.dominguez@leniolabs.com>
2023-05-16 10:48:56 -0400

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwMTEwOQ/spectral.js?branch_slug=master&token=26f9fe7a-b169-4edd-b887-9ce25a514bb3";
+export default { extends: ruleset };
